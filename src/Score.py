4a2b42a20dc8ac2c016037460b5cc6269e4371d7
dillonfeinman <dfeinma1@binghamton.edu>
2016-11-30 16:35:40 -0500

    Update Score.py

--- src/Score.py
+++ src/Score.py
@@ -1,10 +1,17 @@
 import pygame
+import __init__.py  
+
+clock = pygame.time.Clock()
 
 class Score:
   def __init__(self):
-        '''Initializes score'''
-        pygame.sprite.Sprite.__init__(self)
-        self.x = 0
-        self.y = 0
-        self.v = 0
-        self.g = 0
+    self.score = 0
+    self.x = 0
+    self.y = 0
+    self.v = 0
+    self.g = 0
+  def changeScore(score):
+    while(done == False):
+      clock.tick(60)
+      score += (score * .003)
+      print(score)
