41cca81fe468a36c395a190249e8f0da18e0c3bd
Sean Kunz <skunz1@binghamton.edu>
2016-11-30 20:56:10 -0500

    Added test code

---
+++ src/tests.py
@@ -0,0 +1,11 @@
+from Kangaroo import Kangaroo
+
+def main():
+    print("#### Testing Jump ####")
+    test_kang = Kangaroo()
+
+    print("==== Cannot jump multiple times ====")
+    test_kang.jump()
+    print(jumpup)
+
+main()
